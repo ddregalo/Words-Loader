ba810d75cf30a253d70497189e4abb1dc5c76918
danielcampos <ddregalo@gmail.com>
2020-01-25 15:53:06 +0000

    Remove template home view content

---
+++ src/components/WordLoader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WordLoader",
+  props: {
+    words: [String]
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
